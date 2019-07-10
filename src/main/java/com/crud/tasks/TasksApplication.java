374abef8b458385113a1a736ac3b78e0cb76dddd
mohhio <m.dziagacz@gmail.com>
2019-07-10 22:25:30 +0200

    heroku deployment

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -9,7 +9,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication
 //public class TasksApplication extends SpringBootServletInitializer {
-	public class TasksApplication {
+public class TasksApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TasksApplication.class, args);
