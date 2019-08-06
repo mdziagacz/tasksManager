db91e44c47680a7f6c6957b8dfe85f065e0ddc01
mohhio <m.dziagacz@gmail.com>
2019-08-06 22:18:15 +0200

    20.3 cleaning

--- src/test/java/com/crud/tasks/controller/TrelloControllerTest.java
+++ src/test/java/com/crud/tasks/controller/TrelloControllerTest.java
@@ -1,28 +1,28 @@
 package com.crud.tasks.controller;
 
-        import com.crud.tasks.domain.*;
-        import com.crud.tasks.trello.facade.TrelloFacade;
-        import com.google.gson.Gson;
-        import org.junit.Test;
-        import org.junit.runner.RunWith;
-        import org.springframework.beans.factory.annotation.Autowired;
-        import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-        import org.springframework.boot.test.mock.mockito.MockBean;
-        import org.springframework.http.MediaType;
-        import org.springframework.test.context.junit4.SpringRunner;
-        import org.springframework.test.web.servlet.MockMvc;
+import com.crud.tasks.domain.*;
+import com.crud.tasks.trello.facade.TrelloFacade;
+import com.google.gson.Gson;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
-        import java.util.ArrayList;
-        import java.util.List;
+import java.util.ArrayList;
+import java.util.List;
 
-        import static org.hamcrest.Matchers.hasSize;
-        import static org.hamcrest.Matchers.is;
-        import static org.mockito.ArgumentMatchers.any;
-        import static org.mockito.Mockito.when;
-        import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-        import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-        import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-        import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(TrelloController.class)
