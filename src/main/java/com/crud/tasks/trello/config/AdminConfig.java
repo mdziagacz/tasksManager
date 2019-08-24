277d08fbe49b342506fcbf77094961d7f4bf3c05
mohhio <m.dziagacz@gmail.com>
2019-08-24 23:10:25 +0200

    24.2 Thymeleaf - mail template - task

--- src/main/java/com/crud/tasks/trello/config/AdminConfig.java
+++ src/main/java/com/crud/tasks/trello/config/AdminConfig.java
@@ -10,4 +10,19 @@ public class AdminConfig {
 
     @Value("${admin.mail}")
     private String adminMail;
+
+    @Value("${admin.name}")
+    private String adminName;
+
+    @Value("${info.company.name}")
+    private String companyName;
+
+    @Value("${info.company.goal}")
+    private String companyGoal;
+
+    @Value("${info.company.email}")
+    private String companyEmail;
+
+    @Value("${info.company.phone}")
+    private String companyPhone;
 }
